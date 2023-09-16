21217f1b0efaa4ccb65b9f13dc7f583ac28343fd
Safayat-Hasan <safayathasananim20@gmail.com>
2023-09-16 16:13:14 +0600

    Initialized the project with header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-3xl font-bold text-center mt-12 mb-12'>Course Registration</h1>
+      
+    </>
+  )
+}
+
+export default App
