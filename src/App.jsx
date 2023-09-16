3a1419910d9bb83ef25b014a3a3194e2a638f288
Safayat-Hasan <safayathasananim20@gmail.com>
2023-09-16 17:05:09 +0600

    Loaded the data from json and showed length of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import Courses from './components/Courses/Courses'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       
-      <h1 className='text-3xl font-bold text-center mt-12 mb-12'>Course Registration</h1>
+      <Header></Header>
+      <div>
+        <Courses></Courses>
+      </div>
       
     </>
   )
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Courses = () => {
+
+    const [courses, setCourses] = useState([])
+
+    useEffect(()=>{
+        fetch('courses.json')
+        .then(res=>res.json())
+        .then(data=>setCourses(data))
+    },[])
+
+    return (
+        <div>
+            <h1 className="text-4xl">Courses: {courses.length}</h1>
+        </div>
+    );
+};
+
+export default Courses;
