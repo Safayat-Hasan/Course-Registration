a583d700acbb2937f99bd3a991ef987ab4388a7f
Safayat-Hasan <safayathasananim20@gmail.com>
2023-09-16 23:50:00 +0600

    Tried my best but could not complete

--- src/components/Course_cart/Course_cart.jsx
+++ src/components/Course_cart/Course_cart.jsx
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 // import { useState } from 'react';
+import {  ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import showToast from '../Course_carts/Course_carts'
 
 const Course_cart = ({cart}) => {
     const {course_name}=cart;
@@ -9,10 +12,26 @@ const Course_cart = ({cart}) => {
     //     // return index;
     // };
     
+    // const idArray = [];
+
+    // const updateArray = id =>{
+    //     idArray.push(id);
+    //     console.log(idArray);
+    // }
+
+    // updateArray()
+
+    // const showToastTwo = () => {
+    //     toast.error('Cannot add same course twice', {
+    //       position: 'top-center', // Set the toast position
+    //       autoClose: 3000, // Auto close the toast after 3 seconds
+    //     });
+    // };
+    
     return (
         <div className='rounded-xl'>
             <h3 className='text-base text-[#1C1B1B99] ml-6 mb-3'>{course_name}</h3>
-            
+            <ToastContainer />
         </div>
         
     );
--- src/components/Course_carts/Course_carts.jsx
+++ src/components/Course_carts/Course_carts.jsx
@@ -8,7 +8,7 @@ const Course_carts = ({carts, credit}) => {
     
 
     const showToast = () => {
-        toast.error('Cannot add any more courses exceeding 20 credits', {
+        toast.error('Cannot add any more courses', {
           position: 'top-center', // Set the toast position
           autoClose: 3000, // Auto close the toast after 3 seconds
         });
@@ -18,11 +18,11 @@ const Course_carts = ({carts, credit}) => {
 
     return (
         <div className="md:w-1/3 bg-white rounded-xl mb-6">
-            <h2 className="text-lg text-[#2F80ED] ml-6 font-bold mb-5 mt-4">Credit Hour Remaining {20-credit<0?showToast():20-credit} hr</h2>
+            <h2 className="text-lg text-[#2F80ED] ml-6 font-bold mb-5 mt-4">Credit Hour Remaining {20-credit<=0?showToast():20-credit} hr</h2>
             <hr className='mx-6 h-[1px]' />
             <h2 className="text-xl ml-6 font-bold mb-5 mt-4">Course Name</h2>
             {
-                carts.map((cart, idx)=> <Course_cart key={idx} cart={cart}></Course_cart>)
+                carts.map((cart, idx)=> <Course_cart key={idx} cart={cart} credit={credit}></Course_cart>)
             }
             <hr className='mx-6 h-[1px]' />
             <h2 className="text-base font-medium ml-6 mb-5 mt-4">Total Credit Hour : {credit}</h2>
